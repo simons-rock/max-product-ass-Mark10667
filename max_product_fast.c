42111f8946476198a9fa07a516463b47faa0c277
尹秀晨 <yinxiuchen@MacBook-Pro-6.local>
2019-01-27 10:27:30 -0500

    finished

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,19 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int first;
+	int second;
+	int max_product;
+	first = second = 0;
+	max_product = 0;
+	for(int i = 0; i < size; i++){
+		if(first < arr[i]){
+			second = first;
+			first = arr[i];
+		}else if(second < arr[i] && arr[i] != first){
+			second = arr[i];
+		}
+	}
+	max_product = first*second;
+	return max_product;
 }
-
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,13 @@
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int max_product = 0;
+	for(int i = 0; i < size; i++){
+		for(int j = i+1; j < size; j++){
+			if(arr[i]*arr[j] > max_product && arr[i]!=arr[j]){
+				max_product = arr[i]*arr[j];
+			}
+		}
+	}
+	return max_product;
 }
